093d66f04e087e1037f875e77785254fab7e3dac
Ashkan <kalantari@gmail.com>
2018-11-14 10:54:33 +0000

    docz rebuild

--- .docz/app/root.jsx
+++ .docz/app/root.jsx
@@ -5,14 +5,7 @@ import Theme from 'docz-theme-default'
 import { imports } from './imports'
 import db from './db.json'
 
-const Root = () => (
-  <Theme
-    db={db}
-    imports={imports}
-    hashRouter={true}
-    websocketUrl="ws://127.0.0.1:60505"
-  />
-)
+const Root = () => <Theme db={db} imports={imports} hashRouter={true} />
 
 // TODO: this is temporary until react-hot-loader fix hooks issues
 setConfig({
